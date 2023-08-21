0d800bc29f0981e516f7d923aaacae09a10ccd8e
sounwoo <mna800@naver.com>
2023-08-22 01:54:31 +0900

    Docs : 파일 구조 변경

--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -1,5 +1,5 @@
 import { User } from '@prisma/client';
-import prisma from '../../config';
+import prisma from '../../database/prismaConfig';
 import { IUserCreateDTO } from './interfaces/user-service.interface';
 
 export class UserService {
