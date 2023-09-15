bb95fe92aed4e3c31d33893adcd5466fb985065e
jswon-jung <jswon.jung@gmail.com>
2023-09-16 01:45:06 +0900

    Modify: 쿠키 생성 설정 수정

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -3,11 +3,13 @@ import { Response } from 'express';
 export const saveCookie = (res: Response, key: string, value: string) => {
     // 배포 환경
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    const domain = `domain=.allinjob.co.kr; SameSite=None;`;
+    const domain = `domain=.allinjob.co.kr;`;
     res.setHeader(
         'Set-Cookie',
         `${key}=${value};path=/; ${domain} ${
-            key === 'refreshToken' ? 'Secure; httpOnly' : 'Max-Age=3600'
+            key === 'refreshToken'
+                ? ' SameSite=None; Secure; httpOnly'
+                : ' SameSite=Lax; Max-Age=3600'
         }`,
     );
 
