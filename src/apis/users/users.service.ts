3fd4e61f686d81920a648b54eee30482da0aaecf
jswon-jung <jswon.jung@gmail.com>
2023-09-15 18:41:41 +0900

    Modify: 소셜로그인 회원가입 api 수정

--- src/apis/users/users.service.ts
+++ src/apis/users/users.service.ts
@@ -1,9 +1,10 @@
-import { User } from '@prisma/client';
+import { Provider, User } from '@prisma/client';
 import prisma from '../../database/prismaConfig';
 import {
     IUserCreateDTO,
     IUserFindOneUserByID,
 } from './interfaces/user.interface';
+import redis from '../../database/redisConfig';
 
 export class UserService {
     findOneUserByEmail(email: string): Promise<User | null> {
@@ -41,26 +42,53 @@ export class UserService {
     }
 
     async createUser({ createDTO }: IUserCreateDTO): Promise<string> {
-        const { keywords, ...userData } = createDTO;
+        const { interests, ...userData } = createDTO;
 
-        const isEmail = await this.findOneUserByEmail(userData.email);
+        if (!(await redis.get(userData.email)))
+            return '로그인한 이메일과 일치하지 않습니다.';
 
-        if (isEmail) return '이미 존재하는 이메일이 있습니다.';
+        if (await this.findOneUserByEmail(userData.email))
+            return '이미 존재하는 이메일이 있습니다.';
 
-        const create = await prisma.user.create({
-            data: {
-                ...userData,
-                keyword: {
-                    connectOrCreate: keywords.map((keyword) => ({
-                        where: { keyword },
-                        create: { keyword },
-                    })),
+        const create = await prisma.$transaction(async (prisma) => {
+            const provider = await redis.get(userData.email);
+            const user = await prisma.user.create({
+                data: {
+                    ...userData,
+                    provider: provider as Provider,
                 },
-            },
-            include: {
-                keyword: true,
-            },
+            });
+            await Promise.all(
+                interests.map(async (el) => {
+                    const interest = Object.keys(el)[0];
+                    const keywords = Object.values(el)[0];
+                    const createdInterest = await prisma.interest.upsert({
+                        where: { interest },
+                        update: {},
+                        create: { interest },
+                    });
+
+                    return Promise.all(
+                        keywords.map(async (keyword) => {
+                            const createdKeyword = await prisma.keyword.upsert({
+                                where: { keyword },
+                                update: {},
+                                create: { keyword },
+                            });
+                            await prisma.userInterest.create({
+                                data: {
+                                    userId: user.id,
+                                    interestId: createdInterest.id,
+                                    keywordId: createdKeyword.id,
+                                },
+                            });
+                        }),
+                    );
+                }),
+            );
+            await redis.del(userData.email);
+            return user.id;
         });
-        return create.id;
+        return create;
     }
 }
