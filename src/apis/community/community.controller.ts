4a6384092e494ea462c6d59242074f28ecf2779f
sounwoo <mna800@naver.com>
2023-09-22 21:37:27 +0900

    Fix : validate 미들웨어 함수

--- src/apis/community/community.controller.ts
+++ src/apis/community/community.controller.ts
@@ -4,10 +4,12 @@ import { Request, Response, Router } from 'express';
 import { asyncHandler } from '../../middleware/async.handler';
 import accessGuard from '../../middleware/auth.guard/access.guard';
 import { idType, pathType } from '../../common/types';
-import { validateDTO } from '../../common/validator/validateDTO';
+// import { validateDTO } from '../../common/validator/validateDTO';
 import { CreateCommunityDTO } from './dto/create.input';
 import { FindOneCommunityDTO } from './dto/findOneCommunity';
 import { FindManyCommunityDTO } from './dto/findManyCommunity';
+import Validate from '../../common/validator/validateDTO';
+import { next } from 'cheerio/lib/api/traversing';
 
 class CommunityController {
     router = Router();
@@ -21,12 +23,18 @@ class CommunityController {
     init() {
         this.router.post(
             '/create',
+            Validate.createCommunity,
             accessGuard.handle,
             asyncHandler(this.create.bind(this)),
         );
-        this.router.get('/', asyncHandler(this.fidneMany.bind(this)));
+        this.router.get(
+            '/',
+            Validate.findManyCommunity,
+            asyncHandler(this.fidneMany.bind(this)),
+        );
         this.router.get(
             '/:id',
+            Validate.findOneCommunity,
             accessGuard.handle,
             asyncHandler(this.findeOne.bind(this)),
         );
@@ -35,8 +43,6 @@ class CommunityController {
     async create(req: Request, res: Response) {
         const { id } = req.user as idType;
         const createCommunity = req.body;
-
-        await validateDTO(new CreateCommunityDTO(createCommunity));
 
         res.status(200).json({
             data: await this.communityService.create({
@@ -49,11 +55,6 @@ class CommunityController {
     async fidneMany(req: Request, res: Response) {
         const { path } = req.query as pathType;
 
-        path &&
-            (await validateDTO(
-                new FindManyCommunityDTO(req.query as pathType),
-            ));
-
         res.status(200).json({
             data: await this.communityService.findeMany({ path }),
         });
@@ -61,7 +62,6 @@ class CommunityController {
 
     async findeOne(req: Request, res: Response) {
         const { id } = req.params as idType;
-        await validateDTO(new FindOneCommunityDTO(req.params as idType));
 
         res.status(200).json({
             data: await this.communityService.findOne({ id }),
