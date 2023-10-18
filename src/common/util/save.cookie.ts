017a9d04cd5e2582c3a94a1ee15405094919660b
sounwoo <mna800@naver.com>
2023-10-18 19:28:26 +0900

    Docs : cookie test

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -6,15 +6,23 @@ export const saveCookie = (res: Response, key: string, value: string) => {
         // 배포 환경
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
-        const domain = `domain=.allinjob.co.kr;`;
-        res.setHeader(
-            'Set-Cookie',
-            `${key}=${value};path=/; ${domain} ${
-                key === 'refreshToken'
-                    ? ' SameSite=Lax; httpOnly'
-                    : ' SameSite=Lax; Max-Age=3600'
-            }`,
-        );
+        // const domain = 'domain=allinjob.co.kr';
+        // res.setHeader(
+        //     'Set-Cookie',
+        //     `${key}=${value}; path=/; ${domain} ${
+        //         key === 'refreshToken'
+        //             ? 'SameSite=None; Secure; httpOnly'
+        //             : 'SameSite=Lax; Max-Age=3600'
+        //     }`,
+        // );
+        res.cookie(key, value, {
+            // domain: 'allinjob.co.kr',
+            path: '/',
+            sameSite: 'lax',
+            secure: true,
+            httpOnly: true,
+            ...(key !== 'refreshToken' && { maxAge: 3600 }),
+        });
     } else {
         // 로컬 환경
         const domain = `${key}=${value}; path=/;`;
