af7a1120eee2e59fda7f39b65609d7c39bab831c
sounwoo <mna800@naver.com>
2023-10-18 21:55:21 +0900

    Test : cookie test 9

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -6,23 +6,20 @@ export const saveCookie = (res: Response, key: string, value: string) => {
         // 배포 환경
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
-        // const domain = 'domain=allinjob.co.kr';
+        const domain = 'domain=.allinjob.co.kr';
+
+        res.setHeader(
+            'Set-Cookie',
+            `${key}=${value}; path=/; ${domain} ${
+                key === 'refreshToken'
+                    ? 'SameSite=None; Secure; httpOnly'
+                    : 'SameSite=Lax; Max-Age=3600'
+            }`,
+        );
         // res.setHeader(
         //     'Set-Cookie',
-        //     `${key}=${value}; path=/; ${domain} ${
-        //         key === 'refreshToken'
-        //             ? 'SameSite=None; Secure; httpOnly'
-        //             : 'SameSite=Lax; Max-Age=3600'
-        //     }`,
+        //     `refreshToken=${refreshToken};path=/; domain=.sit-woo.store; SameSite=None; Secure; httpOnly`,
         // );
-        res.cookie(key, value, {
-            domain: '.allinjob.co.kr',
-            path: '/',
-            sameSite: 'lax',
-            secure: true,
-            httpOnly: true,
-            ...(key !== 'refreshToken' && { maxAge: 3600 }),
-        });
     } else {
         // 로컬 환경
         const domain = `${key}=${value}; path=/;`;
