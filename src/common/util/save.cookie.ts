a6d4e6d01d1d693c5fc11be6f118cf8886606bcb
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2023-10-18 17:48:14 +0900

    Docs: 쿠키 설정 수정

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -4,6 +4,7 @@ import { url } from './callbackUrl';
 export const saveCookie = (res: Response, key: string, value: string) => {
     if (url().origin) {
         // 배포 환경
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
         const domain = `domain=.allinjob.co.kr;`;
         res.setHeader(
