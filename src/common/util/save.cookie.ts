aa373f3c37f72a1574c87d2eedda2499e46a14d9
박선우 <105111888+sounwoo@users.noreply.github.com>
2023-10-19 16:28:34 +0900

    Test : cookie test

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -6,7 +6,7 @@ export const saveCookie = (res: Response, key: string, value: string) => {
         // 배포 환경
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
-        const domain = `domain=.backendclass.store`;
+        const domain = 'domain=.backendclass.store';
         // res.setHeader(
         //     'Set-Cookie',
         //     `${key}=${value}; path=/; ${domain} ${
@@ -17,9 +17,7 @@ export const saveCookie = (res: Response, key: string, value: string) => {
         // );
         res.setHeader(
             'Set-Cookie',
-            `${key}=${value}; path=/; ${domain}; SameSite=None; Secure; ${
-                key === 'refreshToken' && 'Max-Age=3600;'
-            } httpOnly;`,
+            `${key}=${value}; path=/; ${domain}; SameSite=None; Secure; httpOnly;`,
         );
     } else {
         // 로컬 환경
