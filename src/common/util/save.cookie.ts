c570807df3855bcec9adbae894116bb77d77906e
sounwoo <mna800@naver.com>
2023-10-19 16:17:51 +0900

    Test : cookie test

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -6,14 +6,20 @@ export const saveCookie = (res: Response, key: string, value: string) => {
         // 배포 환경
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
-        const domain = `domain=.backendclass.store;`;
+        const domain = `domain=.backendclass.store`;
+        // res.setHeader(
+        //     'Set-Cookie',
+        //     `${key}=${value}; path=/; ${domain} ${
+        //         key === 'refreshToken'
+        //             ? ' SameSite=None; httpOnly'
+        //             : ' SameSite=Lax; Max-Age=3600'
+        //     }`,
+        // );
         res.setHeader(
             'Set-Cookie',
-            `${key}=${value};path=/; ${domain} ${
-                key === 'refreshToken'
-                    ? ' SameSite=Lax; httpOnly'
-                    : ' SameSite=Lax; Max-Age=3600'
-            }`,
+            `${key}=${value}; path=/; ${domain}; SameSite=None; Secure; ${
+                key === 'refreshToken' && 'Max-Age=3600;'
+            } httpOnly;`,
         );
     } else {
         // 로컬 환경
