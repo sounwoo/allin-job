f5338998a67c9ac0d93ca378dbd0c2ddbd079393
Seungwon Jung <124873708+jswon-jung@users.noreply.github.com>
2023-10-19 16:48:04 +0900

    Test: 쿠키 설정 수정

--- src/common/util/save.cookie.ts
+++ src/common/util/save.cookie.ts
@@ -6,7 +6,7 @@ export const saveCookie = (res: Response, key: string, value: string) => {
         // 배포 환경
         res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
         res.setHeader('Access-Control-Allow-Credentials', 'true');
-        const domain = 'domain=.backendclass.store';
+        const domain = '';
         // res.setHeader(
         //     'Set-Cookie',
         //     `${key}=${value}; path=/; ${domain} ${
