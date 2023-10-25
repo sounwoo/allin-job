6cc2bd97743806cb2264c32c522118aa47bd894c
jswon-jung <jswon.jung@gmail.com>
2023-10-25 13:18:31 +0900

    Feat: finde/language 리턴 데이터 가공 및 수정 & finde/intern organization 리턴 추가

--- src/common/util/return_data_cludes.ts
+++ src/common/util/return_data_cludes.ts
@@ -5,9 +5,9 @@ export const cludes = (path: Path['path'] | 'language'): string[] => {
     const qnetExcept = [...common, 'Dday', 'mainImage', 'institution', 'date'];
     const cludes = {
         ousideOrCompetition: qnetExcept,
-        intern: [...qnetExcept, 'location'],
+        intern: [...qnetExcept, 'location', 'organization'],
         qnet: [...common, 'relatedDepartment', 'institution', 'examSchedules'],
-        language: ['test', 'Dday', 'date'],
+        language: ['test', 'Dday', 'date', 'homePage'],
     };
 
     return cludes[path as 'intern' | 'qnet'] || cludes['ousideOrCompetition'];
